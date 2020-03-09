bf09a0f56216abe2e4f2615127cbb9590a014f7d
ckf48 <3048764886@qq.com>
2020-03-09 10:54:53 +0800

    创建数据库，初始化表结构

--- src/main/java/JDBCTest.java
+++ src/main/java/JDBCTest.java
@@ -2,9 +2,9 @@ import java.sql.*;
 
 public class JDBCTest {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://127.0.0.1:3306/yuri?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT";
+        String url = "jdbc:mysql://127.0.0.1:3306/ckf48?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT";
         String driverName = "com.mysql.cj.jdbc.Driver";
-        String sql = "select * from contract";
+        String sql = "select * from school.s_student";
         try {
             Class.forName(driverName);
             Connection connection = DriverManager.getConnection(url, "root", "123456");
@@ -12,6 +12,10 @@ public class JDBCTest {
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next())
                 System.out.println(resultSet.getInt(1));
+
+            resultSet.close();
+            statement.close();
+            connection.close();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
